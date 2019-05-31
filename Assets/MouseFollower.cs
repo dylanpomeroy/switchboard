ae4aa354aef60fb7bf85ed494dd082e081aa8224
Dylan Pomeroy <dylan.pomeroy.j@gmail.com>
2019-05-31 00:53:02 -0400

    Setup basic cord snapping into plugs on board

---
+++ Assets/MouseFollower.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseFollower : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    Vector3 screenPoint;
+    Vector3 offset;
+
+    private void OnMouseDown()
+    {
+        screenPoint = Camera.main.WorldToScreenPoint(transform.position);
+        offset = transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+
+    }
+
+    Vector3 cursorScreenPoint;
+    Vector3 cursorPosition;
+    private void OnMouseDrag()
+    {
+        cursorScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+        cursorPosition = Camera.main.ScreenToWorldPoint(cursorScreenPoint) + offset;
+        
+        transform.position = cursorPosition;
+    }
+
+    private void OnMouseUp()
+    {
+        var roundedPosition = new Vector3(
+            Mathf.RoundToInt(cursorPosition.x),
+            Mathf.RoundToInt(cursorPosition.y),
+            Mathf.RoundToInt(cursorPosition.z));
+
+        transform.position = roundedPosition;
+    }
+}
